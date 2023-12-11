3511273820e4cb4aba2fd64c314017f895596bf8
egorsurkov33 <egorsurkov83@gmail.com>
2023-12-11 19:44:06 +0300

    EDITING HTML & SCRIPT

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,31 @@
-alert('hello world');
+const apiUrl = 'https://a21455-041c.s.d-f.pw/api/Address/4a162ded-1cff-4603-b217-e9fdbebc7f37';
+
+const requestData = {
+  colorName: 'Black',
+  skuID: 'FF1234567890',
+  releaseDate: '2021-10-17T21:44:16.871Z',
+  country: 'ExampleCountry',
+  city: 'ExampleCity',
+  street: 'ExampleStreet',
+  house: 'ExampleHouse',
+  room: 123 // Замените на нужное значение
+};
+
+const requestOptions = {
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/json'
+  },
+  body: JSON.stringify(requestData)
+};
+
+fetch(apiUrl, requestOptions)
+  .then(response => response.json())
+  .then(data => {
+    console.log('ID созданного адреса:', data);
+    // Дальнейшие действия с данными, если нужно
+  })
+  .catch(error => {
+    console.error('Произошла ошибка:', error);
+    // Обработка ошибки
+  });
